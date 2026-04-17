75d4987e21dd8da1b404ef8114c0311434dac5a8
unknown <238w1a1254@vrsec.ac.in>
2026-04-17 16:18:30 +0530

    update

--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express") 
-const Cart= require("../models/cart")
+const Cart= require("../models/Cart")
 const {protect} = require("../middleware/authMiddleware")
 const router=express.Router() 
 router.post("/add",protect,async (req,res)=>{
