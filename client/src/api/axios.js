6bd9b2dcbe6731df3bc3953b1972586d5b1ba11c
unknown <238w1a1254@vrsec.ac.in>
2026-04-17 16:27:35 +0530

    cart update

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const API = axios.create({
-    baseURL:"http://localhost:5000/api"
+    baseURL:"https://mernecommerce-u8at.onrender.com"
 })
 API.interceptors.request.use((req)=>{
     const token=localStorage.getItem("token")
